a881cb668e0b426798280ee1b581faa6cfce0fc5
pythonstrup <pythonstrup@gmail.com>
2024-01-11 22:56:28 +0900

    Nuxt Project - layout component

--- learn-nuxt/layouts/default.vue
+++ learn-nuxt/layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <h1>{{ $route.name }} 페이지</h1>
     <Nuxt />
   </div>
 </template>
 
 <script>
-
+export default {
+  // create() {
+  //   console.log(this.$route)
+  // }
+}
 </script>
 
 <style>
--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>홈 페이지</h1>
+    <p>Nuxt Start!</p>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+
 </script>
+
+<style>
+
+</style>
