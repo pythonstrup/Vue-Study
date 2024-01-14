ec2854b587fa1700e30fd0dbe890d59420019c8b
pythonstrup <pythonstrup@gmail.com>
2024-01-14 22:50:53 +0900

    Nuxt Project - port 지정 및 data rendering

--- learn-nuxt/pages/main.vue
+++ learn-nuxt/pages/main.vue
@@ -2,6 +2,9 @@
   <div>
     <h1>메인 페이지</h1>
     <p>메인 페이지입니다.</p>
+    <div>
+      {{ products }}
+    </div>
   </div>
 </template>
 
@@ -9,9 +12,16 @@
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
+
   async created() {
-    const response = await axios.get('http://localhost:3000/products')
+    const response = await axios.get('http://localhost:3000/products');
     console.log(response);
+    this.products = response.data;
   }
 }
 
