1b71f20e09909da3f97274df7f1c66196c547360
pythonstrup <pythonstrup@gmail.com>
2024-01-16 22:01:25 +0900

    Nuxt Project - routing styling

--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,14 +1,83 @@
 <template>
-  <div>
-    <h1>홈 페이지</h1>
-    <p>Nuxt Start!</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text"/>
+      </div>
+      <ul>
+        <li v-for="product in products" :key="product.id" class="item flex">
+          <img class="product-image" :src="product.imageUrl" :alt="product.name">
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products');
+    const products = response.data.map(item => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`
+      }
+    });
+    return {products};
+  },
+
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+  //
+  // async created() {
+  //
+  // }
+}
 
 </script>
 
-<style>
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
 
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+
+.app {
+  position: relative;
+}
+
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
 </style>
