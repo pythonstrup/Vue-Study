c0510be039077108bc4b8ba17e2269249cb7cf38
pythonstrup <pythonstrup@gmail.com>
2024-01-19 08:30:48 +0900

    Nuxt Project - 상품 상세 페이지

--- learn-nuxt/pages/detail/_id.vue
+++ learn-nuxt/pages/detail/_id.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <h1>상세 페이지</h1>
+    <div>
+      <img :src="product.imageUrl" :alt="product.name" />
+      <p>name: {{product.name}}</p>
+      <p>price: {{product.price}}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import {fetchProductById} from "~/api";
+
 export default {
-  created() {
-    console.log(this.$route);
-  },
+  async asyncData({ params  }) {
+    const id = params.id;
+    const response = await fetchProductById(id);
+    const product = response.data;
+    return { product };
+  }
+
+  // created() {
+  //   const id = this.$route.params.id;
+  //   const response = fetchProductById(id);
+  // },
 }
 </script>
 
