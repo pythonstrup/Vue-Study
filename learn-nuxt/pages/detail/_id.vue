b0703e19f2c223b89b7a8cb58409ac7eafc54dcd
pythonstrup <pythonstrup@gmail.com>
2024-01-20 15:47:19 +0900

    Nuxt Project - 스타일링

--- learn-nuxt/pages/detail/_id.vue
+++ learn-nuxt/pages/detail/_id.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <h1>상세 페이지</h1>
-    <div>
-      <img :src="product.imageUrl" :alt="product.name" />
-      <p>name: {{product.name}}</p>
-      <p>price: {{product.price}}</p>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+<!--        <button type="button" @click="addToCart">Add to Cart</button>-->
+      </div>
     </div>
   </div>
 </template>
@@ -13,11 +21,11 @@
 import {fetchProductById} from "~/api";
 
 export default {
-  async asyncData({ params  }) {
+  async asyncData({params}) {
     const id = params.id;
     const response = await fetchProductById(id);
     const product = response.data;
-    return { product };
+    return {product};
   }
 
   // created() {
@@ -27,6 +35,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
 </style>
