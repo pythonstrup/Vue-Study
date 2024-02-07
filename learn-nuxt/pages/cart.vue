37daa9006332087f71143278874448615788b168
pythonstrup <pythonstrup@gmail.com>
2024-02-07 22:49:34 +0900

    Nuxt Project - Vuex 장바구니 렌더링

--- learn-nuxt/pages/cart.vue
+++ learn-nuxt/pages/cart.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>카트 페이지입니다.</div>
+  <div>
+    <h1>카트 페이지</h1>
+    <div>
+      <ul>
+        <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id">
+          <img :src="cartItem.imageUrl" :alt="cartItem.name">
+          <p>{{ cartItem.name }}</p>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <button>구매하기</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
