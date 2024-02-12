21a9c359c768d1da5d757070ce3be6f42e9aa753
pythonstrup <pythonstrup@gmail.com>
2024-02-12 12:29:19 +0900

    Nuxt Project - global meta tag

--- learn-nuxt/nuxt.config.js
+++ learn-nuxt/nuxt.config.js
@@ -1,17 +1,20 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'leacn-nuxt',
+    title: 'Nuxt Shopping',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt Shopping Example'
+      },
+      {name: 'format-detection', content: 'telephone=no'},],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -24,10 +27,8 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: [// https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
